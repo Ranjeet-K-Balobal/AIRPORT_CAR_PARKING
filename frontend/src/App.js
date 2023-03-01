e0e1565945c49f9ca26e573d1ac84b54b44affa7
Ranjeet-K-Balobal <ranjeetbalo600@gmail.com>
2023-03-01 18:48:09 +0530

    eag
    dghs

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import './App.css';
+// import './index.css';
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header/>
+      </div>
+      <div>
+      <Homepage/>
+      </div>
+      <div>
+        <Footer/>
+      </div>
+      
+    </div>
+
+    
+  );
+}
+
+export default App;
