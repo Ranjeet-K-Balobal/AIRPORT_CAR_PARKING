9fd3693ec49820badb572a3fab19c460c0acd010
Ranjeet-K-Balobal <ranjeetbalo600@gmail.com>
2023-03-03 19:36:22 +0530

    step 3 Add Usestate

--- frontend/src/pages/Searchform.js
+++ frontend/src/pages/Searchform.js
@@ -1,7 +1,61 @@
+import React,{useState} from "react";
 
-const Searchform = () => {
-    return(
-        <form action="/results.html" method="post" >
+const SearchForm = () => {
+    const[departureAirport,setDepartureAirport ]= useState('Kannur');
+    const[parkingCheckIn,setParkingCheckIn ]= useState('');
+    const[parkingCheckOut,setParkingCheckOut ]= useState('');
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        //i'm printing all the data fetched in console just to verify everything works fine
+        console.log(departureAirport);
+        console.log(parkingCheckIn);
+        console.log(parkingCheckOut);
+    }
+
+    const parkingCheckOutHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckOut(value);
+
+    }
+    const parkingCheckInHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckIn(value);
+    }
+    const departureAirportHandler =(e) => {
+        const {value}=e.target;
+        if(value.length<8){
+            setDepartureAirport(value);
+        }
+    }
+    return (
+        <section id="hero"
+        style={{backgroundImage: 'url("assets/generic_landing.jpg")', minHeight: '500px'}}>
+        <div className="hero-backdrop"></div>
+        <div className="container position-relative">
+            <div className="hero-heading mb-4">
+                <h1>SAVE BIG ON AIRPORT PARKING</h1>
+                <h2>We have the best deals for airport parking lots!</h2>
+            </div>
+            <div className="searchbox landing">
+                <div className="row tabs">
+                    <div className="tab">
+                        <div className="heading">Most Convenient</div>
+                        <div className="button">
+                            <div className="icon"><i className="fas fa-car"></i></div>
+                            Airport Parking Only
+                        </div>
+                    </div>
+                    <div className="tab">
+                        <div className="heading">Best Value</div>
+                        <div className="button">
+                            <div className="icon"><i className="fas fa-bed"></i> + <i
+                                    className="fas fa-car"></i></div>
+                            Hotel &amp; Parking Package
+                        </div>
+                    </div> 
+                </div>
+                <form action="/results.html" method="post" >
                                         <div className="options row m-0"><label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
                                                 <div className="heading mb-1">Departure Airport</div>
                                                 <div className="placeholder placeholder-airport">
@@ -26,6 +80,9 @@ const Searchform = () => {
                                             </div>
                                         </div>
                                     </form>
+     </div>
+        </div>
+    </section>
     );
 }
 export default Searchform;
